93450a40baa6a731e4dfc4939ad052473df9d5d7
MasonMarc <masonmarcantel@gmail.com>
2023-02-06 18:56:27 -0500

    added urls to projects object and updated text

--- portfolio/src/components/Display.js
+++ portfolio/src/components/Display.js
@@ -8,16 +8,21 @@ import Footer from './Footer';
 
 const projects = [
   {
-  title: 'Project 1',
-  description: 'amazing project',
+  title: 'Express Urgent Care',
+  description: 'Urgent care website with calendar',
+  url: 'https://github.com/MasonMarc/Urgent-Care-Appointment-Scheduler',
   },
   {
-    title: 'Project 2',
-    description: 'this is great',
+    title: 'Album Review Generator',
+    description: 'Review generator using node and lasf.fm api',
+      url: 'https://github.com/MasonMarc/Album-Review-Gen',
+
   },
   {
-    title: 'Project 3',
-    description: 'fantastic work',
+    title: 'Note Taker',
+    description: 'Note taking app made using node.js and express',
+      url: 'https://github.com/MasonMarc/Note-Taker',
+
   },
 ];
 
@@ -36,9 +41,9 @@ export default function Display() {
 
 <div class="container px-4 py-5" id="custom-cards">
 <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
-  <Projects title={projects[0].title} description={projects[0].description}/>
-  <Projects title={projects[1].title} description={projects[1].description}/>
-  <Projects title={projects[2].title} description={projects[2].description}/>
+  <Projects title={projects[0].title} description={projects[0].description} url={projects[0].url}/>
+  <Projects title={projects[1].title} description={projects[1].description} url={projects[1].url}/>
+  <Projects title={projects[2].title} description={projects[2].description} url={projects[2].url}/>
   </div>
   </div>
 
--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 export default function Project(props) {
 
+
   return (
       <div class="col">
-        <a href="https://github.com/MasonMarc/React-Portfolio" target="_blank">
+        <a href={props.url} target="_blank">
         <div class="card card-cover h-100 overflow-hidden text-bg-dark rounded-4 shadow-lg">
           <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
             <h3 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold">{props.title}</h3>
