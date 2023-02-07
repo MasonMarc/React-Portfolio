07dd4cd7c65929272d1468348ba42b27132371f5
MasonMarc <masonmarcantel@gmail.com>
2023-02-07 12:35:37 -0500

    added projects

--- portfolio/src/components/Display.js
+++ portfolio/src/components/Display.js
@@ -27,6 +27,27 @@ const projects = [
     photoUrl: '"assets/notetakeSH.png"',
 
   },
+  {
+    title: 'Generate-a-Date',
+    description: 'Generator for date ideas using APIs',
+    url: 'https://github.com/MasonMarc/Generate-a-Date',
+    photoUrl: '"assets/GenDateSH.png"',
+
+  },
+  {
+    title: 'Weather Dashboard',
+    description: 'Weather Dashboard using 5-day weather forecast API',
+    url: 'https://github.com/MasonMarc/Weather-Dashboard',
+    photoUrl: '"assets/WeatherSH.png"',
+
+  },
+  {
+    title: 'Ecommerce Backend',
+    description: 'Back end for E-commerce using Sequelize with MySQL.',
+    url: 'https://github.com/MasonMarc/Ecommerce-Backend',
+    photoUrl: '"assets/Ecomm.gif"',
+
+  },
 ];
 
 export default function Display() {
@@ -57,6 +78,18 @@ export default function Display() {
             description={projects[2].description}
             url={projects[2].url}
             photoUrl={projects[2].photoUrl} />
+          <Projects title={projects[3].title}
+            description={projects[3].description}
+            url={projects[3].url}
+            photoUrl={projects[3].photoUrl} />
+          <Projects title={projects[4].title}
+            description={projects[4].description}
+            url={projects[4].url}
+            photoUrl={projects[4].photoUrl} />
+          <Projects title={projects[5].title}
+            description={projects[5].description}
+            url={projects[5].url}
+            photoUrl={projects[5].photoUrl} />
         </div>
       </div>;
     }
