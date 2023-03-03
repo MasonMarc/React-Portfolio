964fb1dedcde4a4d06a9d9ed888e8b227c1d1cd0
MasonMarc <masonmarcantel@gmail.com>
2023-03-03 14:16:31 -0500

    added updated resume

--- portfolio/src/components/Nav.js
+++ portfolio/src/components/Nav.js
@@ -27,11 +27,6 @@ export default function Nav({ currentPage, handlePageChange }) {
           className={currentPage === 'Projects' ? 'nav-link active' : 'nav-link'}>Projects</a></li>
                 <li className="nav-item mx-0 mx-lg-1">
                   <a className="nav-link py-3 px-0 px-lg-3 rounded"
-                        href="#contact"
-                        onClick={() => handlePageChange('Contact')}
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}>Contact</a></li>
-                <li className="nav-item mx-0 mx-lg-1">
-                  <a className="nav-link py-3 px-0 px-lg-3 rounded"
                         href="#resume"
                         onClick={() => handlePageChange('Resume')}
           className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}>Resume</a></li>
--- portfolio/src/components/Resume.js
+++ portfolio/src/components/Resume.js
@@ -7,7 +7,7 @@ export default function Resume() {
     <h1 className="display-5 fw-bold">Resume:</h1>
     <img className="d-block mx-auto mb-4" src="assets/MasonResume.jpg" alt="Resume" width="800"></img>
     <div className="col-lg-6 mx-auto">
-      <a className="lead mb-4" href='assets/MasonResume.pdf'>Click here to download resume.</a>
+      <a className="lead mb-4" href='assets/BootCampResume.pdf'>Click here to download resume.</a>
     </div>
   </div>
   );
